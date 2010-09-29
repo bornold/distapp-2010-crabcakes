d322a20760bbfb834c487958827e8c426c0cdec8
bjorn.johansson <bjorn.johansson@14ab0105-2c40-bc9f-9e3b-aaa4c96e0bb8>
2010-09-29 20:09:23 +0000

    pillipill

--- workshops/jpa.nbp/src/test/java/edu/chl/johanssb/jpa/WebShopTest.java
+++ workshops/jpa.nbp/src/test/java/edu/chl/johanssb/jpa/WebShopTest.java
@@ -2,38 +2,52 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.chl.johanssb.jpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
  * @author flipmo
  */
-public class WebShopTest extends TestCase {
-    
-    public WebShopTest(String testName) {
-        super(testName);
+public class WebShopTest {
+
+    public WebShopTest() {
     }
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
+    @BeforeClass
+    public static void setUpClass() throws Exception {
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
+    @AfterClass
+    public static void tearDownClass() throws Exception {
     }
 
-    // TODO add test methods here. The name must begin with 'test'. For example:
-    // public void testHello() {}
-    public void testAddProduct(){
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void testAddProduct() {
+        assertTrue(true);
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("webshop_pu_test");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
@@ -50,8 +64,5 @@ public class WebShopTest extends TestCase {
         //assertTrue(newProd.getName().equals(prod.getName()));
         assertTrue(true);
         em.close();
-
-        
     }
-
 }
