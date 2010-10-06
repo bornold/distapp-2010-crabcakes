e0056803c24086f2ad2937c8590d7f184327a159
binbop.se <binbop.se@14ab0105-2c40-bc9f-9e3b-aaa4c96e0bb8>
2010-10-06 13:54:51 +0000

    Lagt till ett JavaScript för att visa samma header på alla sidor samt gjort en skitfin service.xhtml

---
+++ trunk/project/QueerCars/src/main/webapp/JavaScript/showServiceText.js
@@ -0,0 +1,8 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function(){$("#serviceText").load("html_res/serviceText.ihtml");});
+
+
+
