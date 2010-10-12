c4504d21d7d67f4818e4244edc4d01be97e263b9
bjorn.johansson <bjorn.johansson@14ab0105-2c40-bc9f-9e3b-aaa4c96e0bb8>
2010-10-12 13:35:30 +0000

    Påbörjat loginförfarandet.

--- project/QueerCars/src/main/java/edu/chl/queercars/servlets/UserLogin.java
+++ project/QueerCars/src/main/java/edu/chl/queercars/servlets/UserLogin.java
@@ -4,8 +4,6 @@
  */
 package edu.chl.queercars.servlets;
 
-import edu.chl.queercars.Administrator;
-import edu.chl.queercars.Car;
 import edu.chl.queercars.Customer;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,21 +12,20 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
 /**
  *
  * @author johanssb
  */
-@WebServlet(name = "InitServlet", urlPatterns = {"/InitServlet"})
-public class InitServlet extends HttpServlet {
+public class UserLogin extends HttpServlet {
 
     EntityManagerFactory emf;
-
-    /**
+    Customer cust;
+    
+    /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
@@ -40,23 +37,42 @@ public class InitServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
 
+        //Session for tracking the logged in user.
+        HttpSession session = request.getSession();
+
         String action = request.getParameter("action");
+        String username = request.getParameter("username");
         try {
+            if (action.equals("doLogin")) {
+                cust = validateUser(username);
+            }
             out.println("<html>");
             out.println("<head>");
             out.println("<title>Servlet InitServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            if (action == null) {
-                showMenuPage(out);
-            } else if (action.equals("recreate")) {
-                recreateData(out);
+            if(cust != null){
+                out.println("<p>Logged in user: " + cust.getId() + " " + cust.getFname() + "</p>");
+                session.setAttribute("customerId", cust.getId());
+            }
+            else{
+                out.println("<p>Epic fail!</p>");
             }
             out.println("</body>");
             out.println("</html>");
         } finally {
             out.close();
         }
+    }
+
+    private Customer validateUser(String username) {
+        emf = Persistence.createEntityManagerFactory("queercars_pu");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        Customer retcust = em.find(Customer.class, username);
+        em.close();
+        emf.close();
+        return retcust;
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -94,47 +110,4 @@ public class InitServlet extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
-    private void showMenuPage(PrintWriter out) {
-        out.println("<p><a href=\"?action=recreate\">Create awesome boilerplate data for testing.</a></p>");
-    }
-
-    private void recreateData(PrintWriter out) {
-        out.println("<p>Recreating data.</p>");
-        emf = Persistence.createEntityManagerFactory("queercars_pu");
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
-
-        Customer cust1 = new Customer("4412121234","Lennart");
-        Customer cust2 = new Customer("3312121234","Emil");
-        Customer cust3 = new Customer("2212121234","Filip");
-        Customer cust4 = new Customer("1112121234","Jens");
-        Customer cust5 = new Customer("5512121234","Mio");
-        Car car1 = new Car("KKN111");
-        Car car2 = new Car("KKN222");
-        Car car3 = new Car("KKN333");
-        Car car4 = new Car("KKN444");
-        Car car5 = new Car("KKN555");
-        Car car6 = new Car("KKN666");
-        Administrator admin1 = new Administrator("jakob", "Jakob");
-        Administrator admin2 = new Administrator("damien", "Damien");
-
-        tx.begin();
-        em.persist(cust1);
-        em.persist(cust2);
-        em.persist(cust3);
-        em.persist(cust4);
-        em.persist(cust5);
-        em.persist(car1);
-        em.persist(car2);
-        em.persist(car3);
-        em.persist(car4);
-        em.persist(car5);
-        em.persist(car6);
-        em.persist(admin1);
-        em.persist(admin2);
-        tx.commit();
-        em.close();
-        emf.close();
-    }
 }
