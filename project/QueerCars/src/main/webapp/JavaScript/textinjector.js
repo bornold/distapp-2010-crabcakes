f2b3129d9ce0b29f271b817c6bbbac1742589c87
bjorn.johansson <bjorn.johansson@14ab0105-2c40-bc9f-9e3b-aaa4c96e0bb8>
2010-10-12 12:16:01 +0000

    Lite fix...

---
+++ project/QueerCars/src/main/webapp/JavaScript/textinjector.js
@@ -0,0 +1,8 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function(){$("#clubText").load("html_res/clubText.ihtml");});
+$(document).ready(function(){$("#serviceText").load("html_res/serviceText.ihtml");});
+$(document).ready(function(){$("#termsText").load("html_res/termsText.ihtml");});
+$(document).ready(function(){$("#carsText").load("html_res/carsText.ihtml");});
