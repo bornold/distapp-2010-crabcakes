c62d596b8987229ecde2ed8739c333646856df73
bjorn.johansson <bjorn.johansson@14ab0105-2c40-bc9f-9e3b-aaa4c96e0bb8>
2010-10-18 10:26:57 +0000

    Påbörjat dynamisk hämtning av bilar till customer-facing sidan.

--- trunk/project/QueerCars/src/main/webapp/JavaScript/textinjector.js
+++ trunk/project/QueerCars/src/main/webapp/JavaScript/textinjector.js
@@ -2,7 +2,20 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-$(document).ready(function(){$("#clubText").load("html_res/clubText.ihtml");});
-$(document).ready(function(){$("#serviceText").load("html_res/serviceText.ihtml");});
-$(document).ready(function(){$("#termsText").load("html_res/termsText.ihtml");});
-$(document).ready(function(){$("#carsText").load("html_res/carsText.ihtml");});
+$(document).ready(function(){
+    $("#clubText").load("html_res/clubText.ihtml");
+});
+$(document).ready(function(){
+    $("#serviceText").load("html_res/serviceText.ihtml");
+});
+$(document).ready(function(){
+    $("#termsText").load("html_res/termsText.ihtml");
+});
+$(document).ready(function(){
+    $("#carsText").load("html_res/carsText.ihtml", loadCarInfo);
+});
+function loadCarInfo(){
+    $("#carsInfo").load("InformationServlet", {
+        action: "getDetailedCarTable"
+    });
+}
