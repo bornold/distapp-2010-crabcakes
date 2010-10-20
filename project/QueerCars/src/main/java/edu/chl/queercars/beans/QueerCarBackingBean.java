29cc30e36e90929b97762aa323fd9785dc6deaaf
Johonas <Johonas@14ab0105-2c40-bc9f-9e3b-aaa4c96e0bb8>
2010-10-20 11:22:31 +0000

    Jag har ändrat mass grejer, men jag kan inte komma ihåg vad jag gjort...
    Men jag tror nog det är bra, kanske.

--- project/QueerCars/src/main/java/edu/chl/queercars/beans/QueerCarBackingBean.java
+++ project/QueerCars/src/main/java/edu/chl/queercars/beans/QueerCarBackingBean.java
@@ -1,10 +1,12 @@
 package edu.chl.queercars.beans;
 
 import edu.chl.queercars.Customer;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.context.ExternalContext;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -25,6 +27,7 @@ public class QueerCarBackingBean {
     public void setId(String id) {
 	this.id = id;
     }
+
     public LoginModelBean getLoginModelBean() {
 	return loginModelBean;
     }
@@ -37,25 +40,34 @@ public class QueerCarBackingBean {
 
 	UtilityBean ub = new UtilityBean();
 	Customer cust = ub.validateUser(id);
+	ub.closeEm();
+	FacesContext ctx = FacesContext.getCurrentInstance();
 
 	if (cust != null) {
 	    loginModelBean.setId(id);
 	    loginModelBean.setName(cust.getFname());
 	    loginModelBean.setEmail(cust.getEmail());
+	    ctx.addMessage("loggin", new FacesMessage("Login Successfull"));
 	    return "success";
 	} else {
+	    ctx.addMessage("loggin", new FacesMessage("Login unsuccessfull"));
 	    return "failure";
 	}
     }
 
     public String doLogout() {
+	FacesContext ctx = FacesContext.getCurrentInstance();
+	ctx.addMessage("loggin", new FacesMessage("Goodbye " + loginModelBean.getName()));
+	
 	loginModelBean.setId(null);
 	loginModelBean.setName(null);
-	// BehÃ¶vs detta?
+	loginModelBean.setEmail(null);
+	//////Behövs detta?/////
 	HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
 	if (session != null) {
 	    session.invalidate();
 	}
+	////////////////////////
 	return "logout";
     }
 
